62a4eec99c59f158f01a90a546183dcd8e1d7322
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 15:56:18 -0600

    Adds addToHistoryReducer;

---
+++ src/reducers/history.js
@@ -0,0 +1,14 @@
+const addToHistoryReducer = ( state=[], action ) => {
+    switch ( action.type ) {
+        case 'ADD_TO_HISTORY':
+            const newHistory = {
+                history: action.payload
+            }
+            const newHistoryList = [...state];
+            newHistoryList.push( newHistory );
+            return newHistoryList;
+        default:
+            return state;
+    }
+}
+export default addToHistoryReducer;
