0ab0c84ffce25011ee51063105e26d17aad8274a
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 16:30:51 -0600

    Adds states for input1, input2, and operator; Adds onChange for input1, input2, and operator;

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Calculator = () => {
+
+    const [ input1, setInput1 ] = useState(0);
+    const [ operator, setOperator ] = useState( "" );
+    const [ input2, setInput2 ] = useState(0);
+    const [ history, setHistory ] = useState( "" );
+
     return (
         <>
             <form>
                 <label htmlFor="input1">Enter a number:</label>
-                <input id="input1" type="number" />
+                <input id="input1" type="number" value={ input1 } onChange={e => { setInput1( e.target.value )}}/>
 
                 <label htmlFor="operator">Choose an operator:</label>
-                <select id="operator">
+                <select id="operator" value={ operator } onChange={e => { setOperator( e.target.value )}}>
                     <option value="+">+</option>
                     <option value="-">-</option>
                     <option value="*">*</option>
@@ -16,7 +22,7 @@ const Calculator = () => {
                 </select>
 
                 <label htmlFor="input2">Enter another number:</label>
-                <input id="input2" type="number" />
+                <input id="input2" type="number" value={input2} onChange={e => { setInput2( e.target.value )}}/>
 
                 <input type="submit" value="Calculate" />
             </form>
