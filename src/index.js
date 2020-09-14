c691e362ac494867373a33398a22a96afca0be91
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 16:06:41 -0600

    Adds Title component;

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { createStore } from 'redux';
 import { addToHistoryReducer } from './reducers/history';
 import { Provider } from 'react-redux';
+import Title from './components/Title';
 
 
 const store = createStore( addToHistoryReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
@@ -13,7 +14,7 @@ store.subscribe( () => console.log( store.getState() ) );
 ReactDOM.render(
   <Provider store={ store }>
     <>
-
+      <Title title="Krisztina's React Calculator" />
     </>
   </Provider>,
   document.getElementById('root')
