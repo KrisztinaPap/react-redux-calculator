374103747f808e029527b47d38f79be6206c2b78
Krisztina Pap <krisztina.pap@outlook.com>
2021-01-05 23:06:32 -0700

    Began adding functionality to react to change in screen-size (ran out of time, not fully implemented)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/nav.css';
 import { Link } from 'react-router-dom';
 
 function Nav() {
+
+    const [screenSize, setScreenSize] = useState(true);
+    const [windowWidth, setWindowWidth] = useState(0);
+    let resizeWindow = () => {
+        setWindowWidth(window.innerWidth);
+        console.log(windowWidth);
+        checkScreenSize(); 
+    };
+
+    const checkScreenSize = () => {
+        
+        (windowWidth < 600)
+        ? setScreenSize(true)
+        : setScreenSize(false);
+        //console.log(screenSize);
+    }
+
+    useEffect(() => {
+        resizeWindow();
+        console.log("Running useEffect!");
+
+        window.addEventListener("resize", resizeWindow);
+        return () => window.removeEventListener("resize", resizeWindow);
+    }, []);
+
     return (
         <nav id="nav">
+            
             <ul>
+                <button className="hamburger-menu"><i className="fas fa-bars"></i></button>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
