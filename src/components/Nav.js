81a4e0641cc99ebf80b82b91aa342893f5fdd530
Krisztina Pap <krisztina.pap@outlook.com>
2021-01-06 15:19:50 -0700

    Added show/hide toggle to hamburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 function Nav() {
+  
+    const [isOpen, setIsOpen] = useState(false);
+
+    const menuToggle = () => {
+        setIsOpen(!isOpen);
+        console.log(isOpen);
+    };
+
 
     return (
         <nav id="nav">
+       
             <ul id="nav-ul">
                 <li className="nav-button">
-                    <button id="hamburger-menu"><i className="fas fa-bars"></i></button>
+                    <button id="hamburger-menu" onClick={menuToggle}><i className="fas fa-bars"></i></button>
                 </li>
-                <li className="nav-link">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="nav-link">
-                    <Link to="/calculator-one-input">One-input Calculator</Link>
-                </li>
-                <li className="nav-link">
-                    <Link to="/calculator-two-input">Two-input Calculator</Link>
-                </li>
-                <li className="nav-link">
-                    <Link to="/calculator-buttons">Calculator with Buttons</Link>
-                </li>
-                <li className="nav-link">
-                    <Link to="/History">History</Link>
-                </li>
+                
+                { isOpen &&  
+                <>
+                    <li className="nav-link">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="/calculator-one-input">One-input Calculator</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="/calculator-two-input">Two-input Calculator</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="/calculator-buttons">Calculator with Buttons</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="/History">History</Link>
+                    </li>  
+                </>}
             </ul>
         </nav>
     );
